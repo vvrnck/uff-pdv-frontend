0949957082a8cf63dca93f2aca8c3ed998ddb0df
vv <rwndm@hotmail.com>
2020-11-18 00:19:31 -0300

    vuetify

---
+++ src/components/navbar/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+     <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <div>Projeto de Software</div>
+      </div>
+      <v-spacer></v-spacer>
+
+      <v-btn icon @click="changeTheme()">
+        <v-icon>mdi-theme-light-dark</v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "NavBar",
+    methods: {
+        changeTheme() {
+            this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+        }
+    }
+}
+</script>
